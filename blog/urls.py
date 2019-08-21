9bc930b933c01dc8d91e5946cf098496a23a05b0
Timothée Cercueil <warlkiry@gmail.com>
2019-08-21 11:19:42 +0300

    Adding post details, modifying urls and creating base html & css

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url('', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<postpk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    #r'^user/(?P<username>[A-Za-z0-9]+)/$'
 ]
